4129675b772b5fd8bd8fe0be9bc186c8ff713779
Alexandre <alexandreponcet.mail@gmail.com>
2017-03-30 01:45:46 +0200

    Add company relation

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,5 +1,5 @@
-const graphql = require('graphql');
-const axios = require('axios');
+const graphql = require("graphql");
+const axios = require("axios");
 const {
   GraphQLObjectType,
   GraphQLString,
@@ -7,17 +7,35 @@ const {
   GraphQLSchema
 } = graphql;
 
+const CompanyType = new GraphQLObjectType({
+  name: "Company",
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString }
+  }
+});
+
 const UserType = new GraphQLObjectType({
-  name: 'User',
+  name: "User",
   fields: {
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        console.log(parentValue);
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.companyId}`)
+          .then(resp => resp.data);
+      }
+    }
   }
 });
 
 const RootQuery = new GraphQLObjectType({
-  name: 'RootQueryType',
+  name: "RootQueryType",
   fields: {
     user: {
       type: UserType,
